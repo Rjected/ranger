e524b6e97d983e529738383aa55008c89efd8c0c
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-08-01 09:43:42 -0400

    chore(relay): remove middleware and trim deps

--- relay/src/lib.rs
+++ relay/src/lib.rs
@@ -4,9 +4,6 @@ pub use crate::relay::{P2PRelay, P2PRelayError};
 mod p2p_api;
 pub use crate::p2p_api::MempoolListener;
 
-mod middleware;
-pub use crate::middleware::P2PMiddleware;
-
 mod service;
 
 mod peer_chains;
