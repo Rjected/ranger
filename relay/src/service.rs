96c0055f9cffd76b418eb1d15f53aee7a1bda966
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-08-02 23:01:29 -0400

    feat(relay): switch tower service types to ethp2p
     * use `Request` and `Response` instead of `EthMessage`

--- relay/src/service.rs
+++ relay/src/service.rs
@@ -1,5 +1,5 @@
 use crate::{P2PRelay, P2PRelayError};
-use ethp2p::EthMessage;
+use ethp2p::{Request, Response};
 use std::{
     future::Future,
     pin::Pin,
@@ -9,10 +9,10 @@ use std::{
 use tower::Service;
 use tracing::debug;
 
-impl Service<EthMessage> for P2PRelay {
+impl Service<Request> for P2PRelay {
     /// The Response is an Option<EthMessage> because peers can return either a response to a
     /// request, or the message received is a broadcast message which does not require a response.
-    type Response = Option<EthMessage>;
+    type Response = Response;
     type Error = P2PRelayError;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + 'static>>;
 
@@ -21,26 +21,26 @@ impl Service<EthMessage> for P2PRelay {
         Poll::Ready(Ok(()))
     }
 
-    fn call(&mut self, req: EthMessage) -> Self::Future {
+    fn call(&mut self, req: Request) -> Self::Future {
         let status_map = Arc::clone(&self.status_map);
         Box::pin(async move {
             let response = match req {
                 // respond to status messages by default
-                EthMessage::Status(their_status) => {
+                Request::Status(their_status) => {
                     debug!("Decoded status message from peer: {:?}", their_status);
                     let mut unlocked_status = status_map.write();
 
                     // if we can't find the other peer's status, add it to the map so we can send
                     // it to peers later
                     match unlocked_status.get(&u64::from(their_status.chain)) {
-                        Some(&status) => Ok(Some(status.into())),
+                        Some(&status) => Ok(status.into()),
                         None => {
                             unlocked_status.insert(u64::from(their_status.chain), their_status);
-                            Ok(Some(their_status.into()))
+                            Ok(their_status.into())
                         }
                     }
                 }
-                _ => Ok(None),
+                _ => Ok(Response::Nil),
             };
             response
         })
